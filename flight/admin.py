32f904bf95f49084db8aa247e68fd117bc7f603b
raymundo21213 <157451536+raymundo21213@users.noreply.github.com>
2024-04-23 16:17:50 +0800

    Add files via upload

---
+++ flight/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from flight.models import Airport, Flight, Passenger
+
+# Register your models here.
+
+admin.site.register(Airport)
+admin.site.register(Flight)
+admin.site.register(Passenger)
